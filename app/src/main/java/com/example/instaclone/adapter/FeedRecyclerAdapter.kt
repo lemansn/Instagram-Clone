7afac7070a6a0a423b99dad974ca53f7e3b43246
lemansn <lemansultanova02@gmail.com>
2023-02-15 22:29:15 +0400

    add post

---
+++ app/src/main/java/com/example/instaclone/adapter/FeedRecyclerAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.instaclone.adapter
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instaclone.model.Post
+import com.example.instaclone.R
+import com.example.instaclone.databinding.RecyclerViewRowBinding
+import com.squareup.picasso.Picasso
+
+class FeedRecyclerAdapter(private val postList : ArrayList<Post>) : RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder>() {
+
+    class PostHolder(val binding: RecyclerViewRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val binding = RecyclerViewRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PostHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        holder.binding.recyclerEmailText.text = postList.get(position).email
+        holder.binding.recyclerCommentText.text = postList.get(position).comment
+        Picasso.get().load(postList[position].downloadUrl).into(holder.binding.recyclerImageView)
+    }
+
+
+}
